e92b3f0613818b9e4219ae4806af8a11ee8ce5b0
Niloy Hasan Nahid <notecse23@gmail.com>
2026-04-09 15:37:54 +0600

    random and input taking value added

---
+++ Basic/005_randomNumber.py
@@ -0,0 +1,2 @@
+import random
+print(random.randrange(1, 10)) # between 1 to 9
---
+++ Basic/006_input.py
@@ -0,0 +1,13 @@
+userInput = input()
+print(userInput)
+print(type(userInput))
+
+# input take string value
+# if want to take ingeter or float, need to type casting
+
+intValue = int(input())
+print(intValue)
+print(type(intValue))
+floatValue = float(input())
+print(floatValue)
+print(type(floatValue))
