90e034ea293b59c29a83412ad08b41545d145166
Niloy Hasan Nahid <notecse23@gmail.com>
2026-04-23 21:46:12 +0600

    list practice code added

---
+++ Basic/032_list.py
@@ -0,0 +1,22 @@
+# List used to store collections of data
+# structure : variable = [data, data, data]
+# can store different types of data type value
+
+userList = ['Niloy', 'Mukta', 'Irfan', 'Tamanna']
+print(userList)
+
+# list store data with index. it ordered, changeable, removable, allow duplicates
+
+print(userList[2])
+
+# length
+print(len(userList))
+# type
+print(type(userList))
+
+numberList = [4, 23, 5, 2]
+cityList = ["Dhaka", "New York", "Paris", "sydney"]
+print(numberList)
+print(cityList)
+
+# insert item in list
